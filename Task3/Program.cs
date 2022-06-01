24827b3e8bec2025efbd0b019754287d2a599b31
DENIS BLINOV <blinovda1@mail.ru>
2022-06-01 13:05:15 +0300

    add task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,17 @@
-﻿//
+﻿// Напишите программу, которая переводит десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void BinaryNum(int num)
+{
+    string i = "";
+    while (num > 0)
+    {
+        i = num % 2 + i;
+        num = num / 2;
+    }
+    Console.Write(i);
+}
+BinaryNum(45);
+
